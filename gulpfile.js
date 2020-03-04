e2eda625b97073a7f3c101fd6d1129aa5aa2ac51
neages <neages@mail.ru>
2020-03-04 22:16:28 +0300

    feat: Добавил файлы grid, interface, normalize, variables, папку templates с файлом header (все файлы формата *.sass)

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ const sass = require('gulp-sass');
       }
   });
   watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/*.sass", serveSass);
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass(){
-  return src("./scss/*.scss")
+  return src("./sass/*.sass")
   .pipe(sass())
   .pipe(dest("./css"))
   .pipe(browserSync.stream());
