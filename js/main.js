009fa91b2f051fb579d563fd2e903c3ad3dc6c87
neages <neages@mail.ru>
2020-03-11 22:13:31 +0300

    feat: add new controls of modal window

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  modatDialog = document.querySelector('modal__dialog')
+
+
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');}
+
+    modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+    });  
+    closeBtn.addEventListener('click', switchModal)
+    document.addEventListener('keyup', function(event) {
+      modal.classList.remove('modal--visible');
+    })
+    document.addEventListener('click', function(event) {
+      let x = event.target.classList[1];
+      if (x==='modal--visible'){
+        switchModal();
+      };
+    });
+
+  });
+  
+
+
