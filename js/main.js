0f51eafa34d77a3a4c03c6e0cde85918545f1d89
neages <neages@mail.ru>
2020-03-15 14:11:18 +0300

    feat: add projects section and slider

--- js/main.js
+++ js/main.js
@@ -66,6 +66,29 @@ $(document).ready(function () {
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '300');
   });
+
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+    })
+
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination')
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10);
+    bullets.css('left' , prev.width() + 10)
+
 });
 
 
